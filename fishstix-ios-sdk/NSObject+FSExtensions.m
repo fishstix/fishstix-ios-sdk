82a4f134894c06b890d0ed37ce8ff382872fc5fc
fishstix <fishstix@alum.mit.edu>
2012-11-29 00:29:18 -0800

    Block Extension on NSObject

--- fishstix-ios-sdk/NSObject+FSExtensions.m
+++ fishstix-ios-sdk/NSObject+FSExtensions.m
@@ -17,6 +17,8 @@
 
 @implementation NSObject (FSExtensions)
 
+
+// Taken from NSObject+PWObject - https://gist.github.com/1896033
 - (void)performBlock:(void (^)(void))block afterDelay:(NSTimeInterval)delay {
     int64_t delta = (int64_t)(1.0e9 * delay);
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), block);
